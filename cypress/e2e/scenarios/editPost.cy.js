71252b0f9b25fd21e387ca40a7f9e140b2b82305
Camilo Romero <ca.romerom1@uniandes.edu.co>
2023-05-14 16:24:35 -0500

    Include Edit Post screenshots for ghost v3

--- cypress/e2e/scenarios/editPost.cy.js
+++ cypress/e2e/scenarios/editPost.cy.js
@@ -18,7 +18,9 @@ describe("Edit post", () => {
 
     cy.wait(500);
     cy.visit(`${ghostUrl}/ghost/#/posts?type=draft`);
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
     editPostPage.elements.firstPost().click();
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
   });
 
   it("should be able to edit a post title", () => {
@@ -26,30 +28,44 @@ describe("Edit post", () => {
 
     const updatedTitle = `${CONSTANTS.POST_TITLE} Updated`;
     editPostPage.editPost(updatedTitle, "");
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
 
     editPostPage.elements.contentInput().click();
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
 
     editPostPage.elements.navigateToPosts().click();
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
 
+    cy.wait(1000);
     editPostPage.elements.firstPost().should("contain", updatedTitle);
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
   });
 
   it("should be able to edit a post content", () => {
     editPostPage.elements.contentInput().clear();
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
 
     const updatedContent = `${CONSTANTS.POST_CONTENT} Updated`;
     editPostPage.editPost("", updatedContent);
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
     editPostPage.elements.navigateToPosts().click();
+    cy.wait(1000);
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
     editPostPage.elements.firstPost().click();
+    cy.wait(1000);
 
     editPostPage.elements.contentInput().should("contain", updatedContent);
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
   });
 
   it("should change the post title to (Untitled) if it is changed to empty text", () => {
     editPostPage.elements.postTitleInput().clear();
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
 
     editPostPage.elements.contentInput().click();
+    cy.wait(1000);
 
     editPostPage.elements.postTitleInput().should("have.value", "(Untitled)");
+    cy.screenshot(`v3-${Cypress.currentTest.titlePath.join("/")}/step`);
   });
 });
